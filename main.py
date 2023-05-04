ef568c49e00424b9310a6b4bca3a43ec7975c4f9
camel1og <camel.log.2023@gmail.com>
2023-05-05 00:05:10 +0900

    取得した英単語スプレットシートに書き出しOK

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import requests
 import csv
 from bs4 import BeautifulSoup
+import time
 
-
-
+#自作ファイル読み込み
+import ssWrite
 
 #ボット判定回避設定
 USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'
@@ -14,22 +15,31 @@ header = {
 url = 'https://www.eitangokentei.com/chu1-eitango/'
 response = requests.get(url, headers=header)
 soup = BeautifulSoup(response.content, 'html.parser')
-#取得確認
-# print(soup)
+
+table = soup.find('table')#取得データの絞り込み
+tds = []#取得データを最後に格納する変数
+
+#スプレットシート接続
+workbooks = ssWrite.accessSpleadSheet()
+worksheet = workbooks.worksheet('シート1')
+i = 2
+
+for trs in table.find_all('tr'):#for文で英単語を取得
+    time.sleep(3)#書き込みスピードを3秒に1回にする（API制限でエラーになるため）
+    worksheet.update_cell(i,1, trs.find_all("td")[0].text)
+    worksheet.update_cell(i,2, trs.find_all("td")[1].text)
+    i += 1
+    # td = {}#tdのデータを辞書型で格納
 
 
-table = soup.find('table')
-tds = []
-for trs in table.find_all('tr'):
-    td = {}
-    td["英単語"] = trs.find_all("td")[0].text
-    td["日本語"] = trs.find_all("td")[1].text
-    tds.append(td)
+    # td["英単語"] = trs.find_all("td")[0].text
+    # td["日本語"] = trs.find_all("td")[1].text
+    # tds.append(td)
 
 
-with open("English_Word.csv", "w", newline= "") as f:
-    field_name = ["英単語","日本語"]
-    writer = csv.DictWriter(f, fieldnames = field_name)
-    writer.writeheader()
-    writer.writerows(tds)
+# with open("English_Word.csv", "w", newline= "") as f:
+#     field_name = ["英単語","日本語"]
+#     writer = csv.DictWriter(f, fieldnames = field_name)
+#     writer.writeheader()
+#     writer.writerows(tds)
     
